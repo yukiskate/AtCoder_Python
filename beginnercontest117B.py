88687f3ba9e7c19d40ebcec5cb831831661f6ecc
yukiskate <skating_s63august1@yahoo.co.jp>
2019-02-03 23:58:00 +0900

    beginnercontest117B update

--- beginnercontest117B.py
+++ beginnercontest117B.py
@@ -3,11 +3,7 @@ list = list(map(int, input().split()))
 
 new_list = sorted(list, reverse=True)
 
-x = 0
-for i in new_list[1:len(new_list)]:
-    x += i
-
-if new_list[0] < x:
+if new_list[0] < sum(new_list[1:]):
     print('Yes')
 else:
     print('No')
